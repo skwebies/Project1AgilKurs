baab05aac2e51fe2e45e575454325d0db4a8bb1f
Thineskumar Thilakanathan <skwebbies@hotmail.co.uk>
2015-09-18 16:55:44 +0200

    Add to List and show list

--- Labb_1_Agilametoder_SUW/Form1.Designer.cs
+++ Labb_1_Agilametoder_SUW/Form1.Designer.cs
@@ -28,97 +28,140 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.employeeList = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // listBox1
+            // employeeList
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(136, 7);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(650, 472);
-            this.listBox1.TabIndex = 0;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(117, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(13, 40);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(117, 20);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(13, 67);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(117, 20);
-            this.textBox3.TabIndex = 3;
+            this.employeeList.FormattingEnabled = true;
+            this.employeeList.Location = new System.Drawing.Point(236, 7);
+            this.employeeList.Name = "employeeList";
+            this.employeeList.Size = new System.Drawing.Size(550, 433);
+            this.employeeList.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 94);
+            this.button1.Location = new System.Drawing.Point(711, 446);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 57);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // label3
             // 
-            this.button2.Location = new System.Drawing.Point(13, 170);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 62);
-            this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Phone:";
             // 
-            // button3
+            // label2
             // 
-            this.button3.Location = new System.Drawing.Point(13, 376);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 93);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Email:";
             // 
-            // textBox4
+            // label1
             // 
-            this.textBox4.Location = new System.Drawing.Point(13, 350);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(117, 20);
-            this.textBox4.TabIndex = 7;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 25;
+            this.label1.Text = "Name:";
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(37, 247);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(149, 20);
+            this.txt_search.TabIndex = 24;
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(37, 299);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(149, 36);
+            this.btn_search.TabIndex = 23;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(117, 174);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(69, 45);
+            this.btn_show.TabIndex = 22;
+            this.btn_show.Text = "Show";
+            this.btn_show.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(37, 174);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(69, 44);
+            this.btn_add.TabIndex = 21;
+            this.btn_add.Text = "Add";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(37, 137);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(149, 20);
+            this.phone.TabIndex = 20;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(37, 82);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(149, 20);
+            this.email.TabIndex = 19;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(37, 32);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(149, 20);
+            this.name.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(798, 481);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.name);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.employeeList);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Employees";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,14 +169,18 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox employeeList;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox name;
     }
 }
 
--- Labb_1_Agilametoder_SUW/Form1.cs
+++ Labb_1_Agilametoder_SUW/Form1.cs
@@ -16,45 +16,87 @@ namespace Labb_1_Agilametoder_SUW
         public Form1()
         {
             InitializeComponent();
-            Stringlist.Add("Namn     Mejl     Nummer");
-            Stringlist.Add("Bengt     benga55@gmail.com     0701234567");
-            Stringlist.Add("Peter     ptr@hotspot.com     0701234576");
-            Stringlist.Add("Lina     lina_w@gmail.com     0701239654");
-            Stringlist.Add("Ahmed     ahmed_ahmed@gmail.com     0707654321");
-            Stringlist.Add("Angelica     angelangelica@gmail.com     0701212125");
-            Stringlist.Add("Olof     olof_olofsson5@gmail.com     0702223336");
-            Stringlist.Add("Clara     stensson_c@gmail.com     0730456789");
-            Stringlist.Add("Stina     stinastinastinastina@hotmail.com     0700554412");
-            Stringlist.Add("Bertil     beritochbertil@telia.com     0700000258");
+            // Stringlist.Add("Namn     Mejl     Nummer");
+            // Stringlist.Add("Bengt     benga55@gmail.com     0701234567");
+            //  Stringlist.Add("Peter     ptr@hotspot.com     0701234576");
+            //  Stringlist.Add("Lina     lina_w@gmail.com     0701239654");
+            // Stringlist.Add("Ahmed     ahmed_ahmed@gmail.com     0707654321");
+            // Stringlist.Add("Angelica     angelangelica@gmail.com     0701212125");
+            //  Stringlist.Add("Olof     olof_olofsson5@gmail.com     0702223336");
+            // Stringlist.Add("Clara     stensson_c@gmail.com     0730456789");
+            //  Stringlist.Add("Stina     stinastinastinastina@hotmail.com     0700554412");
+             //Stringlist.Add("Bertil     beritochbertil@telia.com     0700000258");
+
+                Stringlist.Add(name.Text + " " + email.Text + " " + phone.Text);
+            
+
+
+
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+
+            if (this.name.Text != "" && this.email.Text!="" && this.phone.Text != "") 
+           {
+                employeeList.Items.Add(name.Text + "     " + email.Text + "     " + phone.Text);
+                Stringlist.Add(name.Text + "     " + email.Text + "     " + phone.Text);
+
+
+            } else {
+                MessageBox.Show("Please enter some text", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.name.Focus();
+                //employeeList.Items.Add(name.Text + "     " + email.Text + "     " + phone.Text);
+                
+
+            }
+            //Stringlist.Add(name.Text + "     " + email.Text + "     " + phone.Text);
+            Stuff();
+        }
+    
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            int i = 0;while(i < Stringlist.Count)
+            {
+                employeeList.Items.Add(Stringlist.ElementAt(i)); i++;
+            }
+       Stuff();
+
+        }
+
+       
+
+        private void Stuff()
+        {
+            name.Clear(); email.Clear(); phone.Clear();
+          
+        }
+
+        private void ClearList()
+        {
+
+            employeeList.Items.Clear();
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+
+            string str = txt_search.Text;int i = 0;
+            while (i < Stringlist.Count)
+            {if (Stringlist.ElementAt(i).Contains(str)){    MessageBox.Show("Hittade: " + Stringlist.ElementAt(i)); i++;}else
+                {MessageBox.Show("Nope"); i++; }}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Stringlist.Add(textBox1.Text +"     "+ textBox2.Text + "     "+ textBox3.Text);
-            Stuff();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int i = 0;while(i < Stringlist.Count)
-            {
-                listBox1.Items.Add(Stringlist.ElementAt(i));i++;
-            }
-            Stuff();
-        }
-        private void Stuff()
-        {
-            textBox1.Clear(); textBox2.Clear(); textBox3.Clear();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-
-            string str = textBox4.Text;int i = 0;
-            while (i < Stringlist.Count)
-            {if (Stringlist.ElementAt(i).Contains(str)){    MessageBox.Show("Hittade: " + Stringlist.ElementAt(i)); i++;}else
-                {MessageBox.Show("Nope"); i++; }}
+           ClearList();
         }
     }
 }
